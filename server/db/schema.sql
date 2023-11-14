56c83a1602086d0f12c6f2e6d9009aa4f6e2af71
KapitanPiratu <janek.marek@outlook.cz>
2023-11-14 21:01:16 +0100

    lecturers schéma

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,4 +1,12 @@
 CREATE TABLE IF NOT EXISTS lecturers (
-    id TEXT PRIMARY KEY,
-    name TEXT
+    uuid TEXT PRIMARY KEY NOT NULL,
+    title_before TEXT,
+    first_name TEXT NOT NULL,
+    middle_name TEXT,
+    last_name TEXT NOT NULL,
+    title_after TEXT,
+    picture_url TEXT,
+    location TEXT,
+    claim TEXT,
+    bio TEXT,
 );
