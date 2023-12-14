8807f7df8ff00c3dba3c1bb988273ec8b15438de
KapitanPiratu <janek.marek@outlook.cz>
2023-12-14 21:06:01 +0100

    přidání tel čísel do GET endpointu + fix emailu

--- server/api/lecturers/index.get.ts
+++ server/api/lecturers/index.get.ts
@@ -6,17 +6,24 @@ export default defineEventHandler((event) => {
         db.all(`
             SELECT
                 lecturers.*,
-                emails.email AS emails
+                json_group_array(DISTINCT emails.email) AS emails,
+                json_group_array(DISTINCT telephone_numbers.number) AS telephone_numbers
             FROM
                 lecturers
             JOIN
                 contact ON lecturers.uuid = contact.lecturer_uuid
             LEFT JOIN
                 emails ON contact.lecturer_uuid = emails.contact_uuid
+            LEFT JOIN
+                telephone_numbers ON contact.lecturer_uuid = telephone_numbers.contact_uuid
             GROUP BY lecturers.UUID
         `, (err, rows) => {
-            console.log(`error: ${err}`)
-            console.log(rows)
+
+            rows.forEach((el: any) => {
+                // json_group_array in sqlite returns array in string, this parses it back to json:
+                el.emails = JSON.parse(el.emails);
+                el.telephone_numbers = JSON.parse(el.telephone_numbers);
+            })
             resolve(rows);
         })
     })
