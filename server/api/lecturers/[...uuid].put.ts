32ea08dd11c4ebc56591d3dde670462cf63ea12a
KapitanPiratu <janek.marek@outlook.cz>
2023-12-20 16:31:55 +0100

    prepsani starych dat do noveho objektu + 404 check

--- server/api/lecturers/[...uuid].put.ts
+++ server/api/lecturers/[...uuid].put.ts
@@ -8,43 +8,56 @@ export default defineEventHandler((event) => {
 
                 const uuid = event.context.params?.uuid;
 
-                db.run(`
-                    UPDATE lecturers
-                    SET
-                        title_before = "${body.title_before}",
-                        first_name = "${body.first_name}",
-                        middle_name = "${body.middle_name}",
-                        last_name = "${body.last_name}",
-                        title_after = "${body.title_after}",
-                        picture_url = "${body.picture_url}",
-                        location = "${body.location}",
-                        claim = "${body.claim}",
-                        bio = "${body.bio}",
-                        price_per_hour = "${body.price_per_hour}"
-                    WHERE uuid = "${uuid}"
-                `, (err => {
-                    if (err) {
-                        setResponseStatus(event, 500);
-                        resolve({ code: 500, message: err });
+                db.get(`SELECT * FROM lecturers WHERE uuid = "${uuid}"`, (err, row: any) => {
+                    if (!row) {
+                        setResponseStatus(event, 404);
+                        resolve({ code: 404, message: 'User not found' })
                     } else {
-                        db.run(`SELECT * FROM lecturers_tags WHERE lecturer_uuid = "${body.uuid}"`, (err: any, rows: any) => {
+                        console.log(body)
+                        Object.keys(row).forEach(key => {
+                            if (!body.hasOwnProperty(key)) body[key] = row[key];
+                        });
+                        console.log(body)
+                        db.run(`
+                            UPDATE lecturers
+                            SET
+                                title_before = "${body.title_before}",
+                                first_name = "${body.first_name}",
+                                middle_name = "${body.middle_name}",
+                                last_name = "${body.last_name}",
+                                title_after = "${body.title_after}",
+                                picture_url = "${body.picture_url}",
+                                location = "${body.location}",
+                                claim = "${body.claim}",
+                                bio = "${body.bio}",
+                                price_per_hour = "${body.price_per_hour}"
+                            WHERE uuid = "${uuid}"
+                        `, (err => {
                             if (err) {
                                 setResponseStatus(event, 500);
                                 resolve({ code: 500, message: err });
                             } else {
-                                if (rows) rows.forEach((el: any) => {
-                                    console.log(el)
-                                    //TODO cases:
-                                    //same
-                                    //new, already in db
-                                    //brand new
-                                    //remove
+                                db.run(`SELECT * FROM lecturers_tags WHERE lecturer_uuid = "${body.uuid}"`, (err: any, rows: any) => {
+                                    if (err) {
+                                        setResponseStatus(event, 500);
+                                        resolve({ code: 500, message: err });
+                                    } else {
+                                        if (rows) rows.forEach((el: any) => {
+                                            console.log(el)
+                                            //TODO cases:
+                                            //same
+                                            //new, already in db
+                                            //brand new
+                                            //remove
+                                        })
+                                        resolve(body)
+                                    }
                                 })
-                                resolve(body)
                             }
-                        })
+                        }))
                     }
-                }))
+                })
+
             })
     })
     return res;
