2ce2c968dd371d26cb8fcf272e13ac6c97869cc1
Pavel Zachariáš <pavel@zacharias.cz>
2024-01-07 18:38:20 +0100

    pridani bloku s nefunkcnim zobrazovani barev

---
+++ components/LecturerBlock.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+const color = defineProps()
+
+onMounted(() => {
+    console.log(color)
+})
+</script>
+
+<template>
+    <div class="block" :class="color">
+        <p>data bloku</p>
+    </div>
+</template>
+
+<style scoped>
+.block {
+    padding: 10rem;
+    margin: 1rem;
+    width: 10rem;
+}
+
+.sunglow {
+    background-color: var(--sunglow);
+}
+
+.firstblock {
+    background-color: var(--sky-blue);
+    padding: 528px;
+    color: var(--jet);
+    width: 295px;
+}
+
+.secondblock {
+    background-color: var(--sunglow);
+    padding: 528px;
+    color: var(--jet);
+    width: 295px;
+}
+
+.thirdblock {
+    background-color: var(--white);
+    padding: 528px;
+    color: var(--jet);
+    width: 295px;
+}
+</style>
--- pages/lecturer/index.vue
+++ pages/lecturer/index.vue
@@ -1,9 +1,41 @@
+<script setup>
+
+const blocks = [
+    {
+        index: 0,
+        color: 'sky-blue'
+    },
+    {
+        index: 1,
+        color: 'sunglow'
+    },
+    {
+        index: 2,
+        color: 'white'
+    }
+]
+
+const lecturers = ref([]);
+
+async function getLecturers() {
+    await $fetch('/api/lecturers', {
+        method: 'get',
+        onResponse(response) {
+            lecturers.value = response.response._data[0] || {};
+        }
+    });
+};
+onMounted(() => {
+    getLecturers();
+});
+</script>
+
 <template>
-    <h1>Lecturer</h1>
+    <LecturerBlock v-for="(block, index) in blocks" :color="block.color" />
 </template>
 
 <style scoped>
-h1 {
-    color: var(--prussian-blue);
+body {
+    background-color: var(--jet);
 }
 </style>
