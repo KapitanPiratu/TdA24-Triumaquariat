e4bb703ef1b90e80fad0c3d49727d33c5e7daeda
FoibosApollon <rkazda.kurim@gmail.com>
2024-01-08 19:26:28 +0100

    další už něco lepší

--- pages/lecturer/index.vue
+++ pages/lecturer/index.vue
@@ -16,23 +16,31 @@ onMounted(() => {
 </script>
 
 <template>
-    <div v-for="lecturer in lecturers">
-        <h1>{{ lecturer.title_before }}</h1>
-        <h1>{{ lecturer.firts_name }}</h1>
-        <h1>{{ lecturer.middle_name }}</h1>
-        <h1>{{ lecturer.last_name }}</h1>
-        <h1>{{ lecturer.title_after }}</h1>
-        <h1>{{ lecturer.picture_url }}</h1>
+    <div class="lecturer-card" v-for="lecturer in lecturers">
+        <div>
+            <img :src="lecturer.picture_url" alt="tda image">
+            <h1>{{ lecturer.title_before }}</h1>
+            <h1>{{ lecturer.first_name }}</h1>
+            <h1>{{ lecturer.middle_name }}</h1>
+            <h1>{{ lecturer.last_name }}</h1>
+            <h1>{{ lecturer.title_after }}</h1>
+        </div>
+        <h1 class="lecturer-claim">{{ lecturer.claim }}</h1>
         <h1>{{ lecturer.location }}</h1>
-        <h1>{{ lecturer.claim }}</h1>
-        <h1>{{ lecturer.bio }}</h1>
         <br>
     </div>
 </template>
 
 <style scoped>
-h1 {
+.lecturer-card {
     padding: 1rem;
     background-color: var(--sky-blue);
+    height: 37vw;
+    width: 25vw;
+}
+
+.lecturer-claim {
+    padding: auto;
+    font-size: larger;
 }
 </style>
