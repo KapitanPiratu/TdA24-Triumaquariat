82fa2591c70c4019bc2c3d7d0a780a990fa184d8
FoibosApollon <rkazda.kurim@gmail.com>
2024-01-08 13:41:20 +0100

    první natstylování

--- pages/lecturer/index.vue
+++ pages/lecturer/index.vue
@@ -5,7 +5,7 @@ async function getLecturers() {
     await $fetch('/api/lecturers', {
         method: 'get',
         onResponse(response) {
-            lecturers.value = response.response._data[0] || {};
+            lecturers.value = response.response._data || [];
         }
     });
 };
@@ -16,5 +16,23 @@ onMounted(() => {
 </script>
 
 <template>
-    <p>{{ lecturers }}</p>
+    <div v-for="lecturer in lecturers">
+        <h1>{{ lecturer.title_before }}</h1>
+        <h1>{{ lecturer.firts_name }}</h1>
+        <h1>{{ lecturer.middle_name }}</h1>
+        <h1>{{ lecturer.last_name }}</h1>
+        <h1>{{ lecturer.title_after }}</h1>
+        <h1>{{ lecturer.picture_url }}</h1>
+        <h1>{{ lecturer.location }}</h1>
+        <h1>{{ lecturer.claim }}</h1>
+        <h1>{{ lecturer.bio }}</h1>
+        <br>
+    </div>
 </template>
+
+<style scoped>
+h1 {
+    padding: 1rem;
+    background-color: var(--sky-blue);
+}
+</style>
