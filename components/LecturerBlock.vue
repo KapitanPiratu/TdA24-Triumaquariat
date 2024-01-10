bfe7539820a91ac804d28a3a95fe4f662477cbb3
Pavel Zachariáš <pavel@zacharias.cz>
2024-01-10 19:04:03 +0100

    Už jenom dostylovat data v blocích

--- components/LecturerBlock.vue
+++ components/LecturerBlock.vue
@@ -1,15 +1,32 @@
 <script setup>
 
-const { color } = defineProps(['color']);
+const props = defineProps(['color', 'display', 'lecturer']);
 
 onMounted(() => {
-    console.log(color)
+    // console.log(props.lecturer)
 })
+// let new_telephone_numbers = props.lecturer.telephone_numbers;
 </script>
 
 <template>
-    <div class="block" :class="color">
-        <p>{{ lecturers }}</p>
+    <div class="block" :class="props.color">
+        <div v-if="props.display.includes('names')">
+            <h1>{{ props.lecturer.title_before }} {{ props.lecturer.first_name }} {{ props.lecturer.middle_name }} {{
+                props.lecturer.last_name }} {{ props.lecturer.title_after }}</h1>
+            <img :src="props.lecturer.picture_url" alt="tda image" />
+        </div>
+        <div v-if="props.display.includes('personaldata')">
+            <div v-html="props.lecturer.bio"></div>
+        </div>
+        <div v-if="props.display.includes('contact')">
+            <div>
+                <p>{{ props.lecturer.location }}</p>
+                <p>{{ props.lecturer.price_per_hour }} Kč/h</p>
+                <p v-for="number in props.lecturer.telephone_numbers">{{ number }}</p>
+                <p v-for="email in props.lecturer.emails">{{ email }}</p>
+            </div>
+        </div>
+        {{ lecturers }}
     </div>
 </template>
 
@@ -19,6 +36,7 @@ onMounted(() => {
 }
 
 .block {
+    position: static;
     display: inline-block;
     height: 40vw;
     width: 23vw;
@@ -27,16 +45,19 @@ onMounted(() => {
 
 .sunglow {
     background-color: var(--sunglow);
-    margin-left: 7.5vw;
+    margin-left: 37.5vw;
+    position: absolute;
 }
 
 .sky-blue {
     background-color: var(--sky-blue);
     margin-left: 7.5vw;
+    position: absolute;
 }
 
 .prussian-blue {
     background-color: var(--prussian-blue);
-    margin-left: 7.5vw;
+    margin-left: 67.5vw;
+    position: absolute;
 }
 </style>
--- pages/lecturer/index.vue
+++ pages/lecturer/index.vue
@@ -3,15 +3,24 @@
 const blocks = [
     {
         index: 0,
-        color: 'sky-blue'
+        color: 'sky-blue',
+        display: [
+            'names'
+        ]
     },
     {
         index: 1,
-        color: 'sunglow'
+        color: 'sunglow',
+        display: [
+            'personaldata'
+        ]
     },
     {
         index: 2,
-        color: 'prussian-blue'
+        color: 'prussian-blue',
+        display: [
+            'contact'
+        ]
     }
 ]
 
@@ -22,16 +31,21 @@ async function getLecturers() {
         method: 'get',
         onResponse(response) {
             lecturers.value = response.response._data[0] || {};
+            console.log(lecturers.value);
         }
     });
 };
 onMounted(() => {
     getLecturers();
 });
+computed: {
+
+};
 </script>
 
 <template>
     <div class="blocks-container">
-        <LecturerBlock v-for="(block, index) in blocks" :color="block.color" />
+        <LecturerBlock v-for="(block, index) in blocks" :color="block.color" :display="block.display"
+            :lecturer="lecturers" />
     </div>
 </template>
