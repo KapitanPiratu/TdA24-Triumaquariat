f528ebedab14db871d9e3783cc237fb90a9798a3
FoibosApollon <rkazda.kurim@gmail.com>
2024-01-10 16:33:34 +0100

    ready jedna karta (bez tagů)

--- pages/lecturer/index.vue
+++ pages/lecturer/index.vue
@@ -17,16 +17,16 @@ onMounted(() => {
 
 <template>
     <div class="lecturer-card" v-for="lecturer in lecturers">
-        <div>
-            <img :src="lecturer.picture_url" alt="tda image">
-            <h1>{{ lecturer.title_before }}</h1>
-            <h1>{{ lecturer.first_name }}</h1>
-            <h1>{{ lecturer.middle_name }}</h1>
-            <h1>{{ lecturer.last_name }}</h1>
-            <h1>{{ lecturer.title_after }}</h1>
+        <img :src="lecturer.picture_url" alt="tda image" class="lecturer-picture">
+        <div class="lecturer-basic">
+            <h1>{{ lecturer.title_before }}
+                {{ lecturer.first_name }}
+                {{ lecturer.middle_name }}
+                {{ lecturer.last_name }}
+                {{ lecturer.title_after }}</h1>
         </div>
         <h1 class="lecturer-claim">{{ lecturer.claim }}</h1>
-        <h1>{{ lecturer.location }}</h1>
+        <h1 class="lecturer-location">{{ lecturer.location }}</h1>
         <br>
     </div>
 </template>
@@ -35,12 +35,28 @@ onMounted(() => {
 .lecturer-card {
     padding: 1rem;
     background-color: var(--sky-blue);
-    height: 37vw;
+    height: 40vw;
     width: 25vw;
 }
 
 .lecturer-claim {
-    padding: auto;
+    padding: 2vw;
     font-size: larger;
+}
+
+.lecturer-location {
+    padding-left: 10vw;
+    font-size: larger;
+}
+
+.lecturer-basic {
+    font-size: 0.75rem;
+    padding-right: 1.3vw;
+    padding-top: 2vw;
+}
+
+.lecturer-picture {
+    padding-left: 3.7vw;
+    padding-top: 2vw;
 }
 </style>
