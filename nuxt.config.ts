7e19ace9d477bf310b15675264f39c6ab3002e55
KapitanPiratu <janek.marek@outlook.cz>
2024-01-13 21:25:25 +0100

    přidání fontů přes google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     '@/assets/css/index.css',
     '@/assets/css/variables.css'
-  ]
+  ],
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      "Lalezar": true,
+      "Open Sans": true
+    }
+  }
 })
