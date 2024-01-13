f02ca513af95e1e6384cce30273a4c8b681e3c7c
KapitanPiratu <janek.marek@outlook.cz>
2024-01-13 21:25:58 +0100

    základní navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar">
+    </div>
+</template>
+
+<style scoped>
+.navbar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 10vh;
+    max-width: 100%;
+    background-color: var(--jet);
+}
+</style>
