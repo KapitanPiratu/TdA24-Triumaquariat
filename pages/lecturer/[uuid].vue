852210a55eee3426bec13a9585393bc1a636b1bb
KapitanPiratu <janek.marek@outlook.cz>
2024-01-14 11:37:31 +0100

    stránka lektora přes uuid

---
+++ pages/lecturer/[uuid].vue
@@ -0,0 +1,65 @@
+<script setup>
+
+const blocks = [
+    {
+        index: 0,
+        color: 'sky-blue',
+        display: [
+            'names'
+        ]
+    },
+    {
+        index: 1,
+        color: 'sunglow',
+        display: [
+            'personaldata'
+        ]
+    },
+    {
+        index: 2,
+        color: 'prussian-blue',
+        display: [
+            'contact'
+        ]
+    }
+]
+
+const lecturers = ref([]);
+
+const route = useRoute();
+
+async function getLecturers() {
+    await $fetch(`/api/lecturers/${route.params.uuid}`, {
+        method: 'get',
+        onResponse(response) {
+            lecturers.value = response.response._data || {};
+            console.log(lecturers.value);
+        }
+    });
+};
+onMounted(() => {
+    getLecturers();
+});
+computed: {
+
+};
+</script>
+
+<template>
+    <div class="blocks-container">
+        <LecturerBlock v-for="(block, index) in blocks" :color="block.color" :display="block.display"
+            :lecturer="lecturers" />
+    </div>
+</template>
+
+<style scoped>
+.blocks-container {
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    top: 13vh;
+    width: 100vw;
+    max-width: 100%;
+    height: 87vh;
+}
+</style>
