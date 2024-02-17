74ee6ab41b6bd76ca6861345d46cca8d98adad5a
KapitanPiratu <janek.marek@outlook.cz>
2024-02-17 21:16:08 +0100

    funkce menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,5 +1,32 @@
+<script setup>
+const showMenu = ref(false);
+
+function hideMenu() {
+    showMenu.value = false;
+};
+</script>
+
 <template>
-    <div class="menu-button">
+    <div class="menu-list" :class="{ shown: showMenu }">
+
+        <nuxt-link @click="hideMenu" class="link" to="/">
+            <img class="icon" src="~/assets/svg/home.svg" alt="home svg">
+            <p>Domů</p>
+        </nuxt-link>
+
+        <nuxt-link @click="hideMenu" class="link" to="/#cards-container">
+            <img class="icon" src="~/assets/svg/lector.svg" alt="home svg">
+            <p>Seznam lektorů</p>
+        </nuxt-link>
+
+        <nuxt-link @click="hideMenu" class="link" to="/login">
+            <img class="icon" src="~/assets/svg/lecturer_login.svg" alt="home svg">
+            <p>Přihlášení pro lektory</p>
+        </nuxt-link>
+
+    </div>
+
+    <div class="menu-button" @click="showMenu = !showMenu">
         <img class="menu-icon" ref="svg" src="~/assets/svg/menu.svg" alt="menu icon">
         <p class="menu-text">Menu</p>
     </div>
@@ -11,7 +38,6 @@ img {
 }
 
 .menu-icon {
-    /* margin-left: 3vw; */
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
@@ -24,7 +50,6 @@ img {
     transform: translateY(-54%);
     font-size: 1.5rem;
     left: 4vw;
-    /* font-weight: bold; */
 }
 
 .menu-button {
@@ -34,5 +59,53 @@ img {
     position: absolute;
     left: 1.5vw;
     margin: 0.5vh;
+}
+
+.menu-list {
+    position: absolute;
+    top: 6.5vh;
+
+    width: 20vw;
+    height: auto;
+
+    background-color: var(--white);
+    box-shadow: 1px 1px 3px var(--jet);
+
+    display: none;
+    grid-template-columns: 1fr;
+}
+
+.shown {
+    display: grid;
+}
+
+.link {
+    position: relative;
+
+    margin: 2vh;
+    margin-left: 2.7vw;
+
+    height: 5vh;
+
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.link * {
+    display: inline;
+}
+
+.icon {
+    height: 5vh;
+    width: auto;
+}
+
+.menu-list p {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 3vw;
+
+    font-size: 1.4rem;
 }
 </style>
