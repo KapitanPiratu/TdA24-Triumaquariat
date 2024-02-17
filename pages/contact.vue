b9d47712a77b0cd4e08c22aa82a482fb4dee8b93
KapitanPiratu <janek.marek@outlook.cz>
2024-02-17 20:00:30 +0100

    pár nudných stránek, zatím s lorem ipsum

---
+++ pages/contact.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <h1>Kontakt</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer rutrum, orci vestibulum ullamcorper ultricies,
+            lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Duis sapien nunc, commodo et, interdum
+            suscipit, sollicitudin et, dolor. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            hymenaeos. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Temporibus autem quibusdam et
+            aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
+            recusandae. Proin in tellus sit amet nibh dignissim sagittis. Nam quis nulla. Neque porro quisquam est, qui
+            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt
+            ut labore et dolore magnam aliquam quaerat voluptatem.
+        </p>
+    </div>
+
+    <Footer class="footer" />
+</template>
+
+<style scoped>
+.container {
+    position: relative;
+    top: 21.5vh;
+
+    margin: 2vh;
+    padding: 2vh;
+
+    background-color: var(--sky-blue);
+    border-radius: 6.5px;
+}
+
+h1 {
+    font-size: 3rem;
+}
+
+p {
+    margin-bottom: 2vh;
+}
+
+.footer {
+    position: absolute;
+}
+</style>
---
+++ pages/registration_info.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <h1>Podmínky registrace</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer rutrum, orci vestibulum ullamcorper ultricies,
+            lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Duis sapien nunc, commodo et, interdum
+            suscipit, sollicitudin et, dolor. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            hymenaeos. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Temporibus autem quibusdam et
+            aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
+            recusandae. Proin in tellus sit amet nibh dignissim sagittis. Nam quis nulla. Neque porro quisquam est, qui
+            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt
+            ut labore et dolore magnam aliquam quaerat voluptatem.
+        </p>
+        <p>
+            Duis pulvinar. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Phasellus enim erat,
+            vestibulum vel, aliquam a, posuere eu, velit. Nullam at arcu a est sollicitudin euismod. Class aptent taciti
+            sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Sed vel lectus. Donec odio tempus
+            molestie, porttitor ut, iaculis quis, sem. Aliquam erat volutpat. Etiam posuere lacus quis dolor. Curabitur
+            vitae diam non enim vestibulum interdum. Vivamus luctus egestas leo. Sed vel lectus. Donec odio tempus molestie,
+            porttitor ut, iaculis quis, sem. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Vestibulum
+            erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Duis sapien nunc, commodo et, interdum suscipit,
+            sollicitudin et, dolor. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem
+            purus in lacus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Praesent in mauris eu tortor
+            porttitor accumsan. Integer imperdiet lectus quis justo.
+        </p>
+        <p>
+            In rutrum. Fusce nibh. Nulla pulvinar eleifend sem. Sed elit dui, pellentesque a, faucibus vel, interdum nec,
+            diam. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Aenean placerat. Integer imperdiet lectus
+            quis justo. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Mauris elementum mauris vitae tortor.
+            Curabitur vitae diam non enim vestibulum interdum. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.
+            In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Donec
+            ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Nulla non lectus sed nisl molestie malesuada. Duis
+            bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Curabitur
+            ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Ut enim ad minima veniam, quis nostrum
+            exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        </p>
+    </div>
+
+    <Footer class="footer" />
+</template>
+
+<style scoped>
+.container {
+    position: relative;
+    top: 21.5vh;
+
+    margin: 2vh;
+    padding: 2vh;
+
+    background-color: var(--sky-blue);
+    border-radius: 6.5px;
+}
+
+h1 {
+    font-size: 3rem;
+}
+
+p {
+    margin-bottom: 2vh;
+}
+
+.footer {
+    position: absolute;
+}
+</style>
