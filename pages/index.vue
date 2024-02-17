e17ceb519568269b076df5572316274a93112311
KapitanPiratu <janek.marek@outlook.cz>
2024-02-17 18:11:43 +0100

    další balík ui změn - část zobrazení lektorů

--- pages/index.vue
+++ pages/index.vue
@@ -19,72 +19,35 @@ onMounted(() => {
     <Welcome />
 
     <div id="cards-container" class="cards-container">
-        <div class="lecturer-card-wrapper" v-for="lecturer in lecturers">
-            <nuxt-link class="link" :to="'/lecturer/' + lecturer.uuid">
-                <div class="lecturer-card">
-                    <Tags :tags="lecturer.tags" :margin="true" />
-                    <div :class="{ 'lecturer-card-absolute': lecturer.tags[0].uuid }">
-                        <img v-if="lecturer.picture_url" :src="lecturer.picture_url" alt="tda image"
-                            class="lecturer-picture">
-                        <img v-else src="~/assets/svg/tda_studium.svg" alt="tda image"
-                            class="lecturer-picture default-picture">
-                        <div class="lecturer-basic">
-                            <h1>{{ lecturer.title_before }}
-                                {{ lecturer.first_name }}
-                                {{ lecturer.middle_name }}
-                                {{ lecturer.last_name }}
-                                {{ lecturer.title_after }}</h1>
-                        </div>
-                        <div class="location-price-wrapper">
-                            <h2 class="lecturer-location">{{ lecturer.location }}</h2>
-                            <h2 class="lecturer-price">{{ lecturer.price_per_hour }}&nbsp;Kč/h</h2>
-                        </div>
-                        <p class="lecturer-claim">{{ lecturer.claim }}</p>
-                        <br>
-                    </div>
-                </div>
-            </nuxt-link>
-        </div>
+        <LecturerCard v-for="lecturer in lecturers" :lecturer="lecturer" />
     </div>
 </template>
 
 <style scoped>
-.lecturer-card {
-    padding: 1rem;
-    background-color: var(--sky-blue);
-    height: 70vh;
-    width: 21vw;
-    border-radius: 6.5px;
-    text-align: center;
-    position: relative;
-    transition-duration: 0.5s;
-}
-
+/* 
 .lecturer-card-absolute {
     position: absolute;
     top: 10vh;
     left: 50%;
     transform: translateX(-50%);
-}
+} */
 
-.lecturer-card-wrapper {
-    margin: 1.5vw;
-}
-
-.link {
-    text-decoration: none;
-}
 
 .cards-container {
-    display: flex;
-    flex-wrap: wrap;
     position: absolute;
     top: 100vh;
-    width: 100vw;
+    left: 50%;
+    transform: translateX(-50%);
+
+    display: flex;
+    flex-wrap: wrap;
+
+    width: 65vw;
     min-height: 100vh;
     max-width: 100%;
 }
 
+/* 
 .lecturer-claim {
     font-size: larger;
 }
@@ -121,5 +84,5 @@ onMounted(() => {
 
 .lecturer-card:hover {
     box-shadow: 10px 10px var(--prussian-blue);
-}
+} */
 </style>
