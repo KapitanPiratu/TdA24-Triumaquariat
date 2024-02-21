4073dc133acf091e8a4b24595d35d19b7727de75
Pavel Zachariáš <pavel@zacharias.cz>
2024-02-21 17:42:01 +0100

    první dashboard, nefunkční karty

---
+++ components/DashboardSubcards.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+const dashdata = defineDashdata(['color', 'display']);
+
+onMounted(() => {
+})
+</script>
+
+<template>
+    <div class="NázevVymyslím">
+        <div class="FirstDashboardSubcard">
+            {{ name }}
+            {{ email }}
+            {{ phone }}
+        </div>
+        <div class="SecondDashboardSubcard">
+            {{ date }}
+            {{ time_from }}
+            {{ time_to }}
+            {{ place }}
+        </div>
+        <div class="ThirdDashboardSubcard">
+            {{ tags }}
+        </div>
+        <div class="FourthDashboardSubcard">
+            {{ comment }}
+        </div>
+    </div>
+</template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,4 +1,36 @@
 <script setup>
+
+const DashboardSubcardData = [
+    {
+        index: 0,
+        color: 'white',
+        display: [
+            'names'
+        ]
+    },
+    {
+        index: 1,
+        color: 'white',
+        display: [
+            'timedata'
+        ]
+    },
+    {
+        index: 2,
+        color: 'white',
+        display: [
+            'tags'
+        ]
+    },
+    {
+        index: 3,
+        color: 'white',
+        display: [
+            'comment'
+        ]
+    }
+]
+
 const validToken = ref(false);
 
 async function validateToken() {
@@ -19,11 +51,12 @@ onMounted(() => {
     validateToken();
 })
 </script>
-
 <template>
     <div class="dashboard-container">
         <div v-if="validToken">
-            <p>Dashboard</p>
+            dashboard
+            <DashboardSubcardBlock v-for="(DashboardDataCard, index) in DashboardSubcards" :color="DashboardSubcards.color"
+                :display="DashboardSubcards.display" />
         </div>
         <div v-else>
             <p>please <NuxtLink to="/login">log in</NuxtLink> first</p>
