46e40a1977db73f7bf5330bfa3cf5a67983eb619
KapitanPiratu <janek.marek@outlook.cz>
2024-02-23 21:11:47 +0100

    úprava rozložení rezervačního dialogu

--- components/ReservationButton.vue
+++ components/ReservationButton.vue
@@ -1,6 +1,6 @@
 <script setup>
 const props = defineProps(['lecturer']);
-
+const formDisabled = ref(false);
 const reservationDialog = useModel(false);
 
 //inputs
@@ -77,8 +77,10 @@ const form = ref(null);
 async function postReservation(e) {
     e.preventDefault();
 
+
     const { valid } = await form.value.validate();
     if (valid) {
+        formDisabled.value = true;
         await $fetch(`/api/reservation`, {
             method: 'post',
             body: {
@@ -91,6 +93,14 @@ async function postReservation(e) {
                 comment: comment.value || '',
                 tags: tagsSelected.value,
                 lecturer_uuid: props.lecturer.uuid
+            },
+            onResponse(response) {
+                if (response.response.status == 200) {
+                    form.value.reset();
+                    formDisabled.value = false;
+                } else {
+                    alert('Something went wrong');
+                }
             }
         })
     }
@@ -110,7 +120,7 @@ async function postReservation(e) {
             </v-card-title>
 
             <v-card-item class="card-item">
-                <v-form class="form" ref="form" @submit.prevent="postReservation">
+                <v-form class="form" ref="form" @submit.prevent="postReservation" :disabled="formDisabled">
 
                     <div class="form-container">
                         <v-text-field v-model="name" :rules="rules" class="dialog-input" label="Jméno"></v-text-field>
@@ -120,55 +130,60 @@ async function postReservation(e) {
                     </div>
 
                     <div class="form-container">
-                        <v-text-field class="dialog-input date-input" v-model="formatedDate" :rules="rules"
-                            label="Vyber datum" readonly @click="menu = !menu"></v-text-field>
-
-                        <v-dialog v-model="menu" class="dialog">
-                            <v-card class="date-card">
-                                <v-date-picker @click="formatDate()" v-model="date" v-if="menu" type="time"
-                                    class="date-picker" color="#ddd"></v-date-picker>
-                            </v-card>
-                        </v-dialog>
 
                         <div class="times-container">
+                            <v-text-field class="dialog-input date-input" v-model="formatedDate" :rules="rules"
+                                label="Vyber datum" readonly @click="menu = !menu"></v-text-field>
+
+                            <v-dialog v-model="menu" class="dialog">
+                                <v-card class="date-card">
+                                    <v-date-picker @click="formatDate()" v-model="date" v-if="menu" type="time"
+                                        class="date-picker" color="#ddd"></v-date-picker>
+                                </v-card>
+                            </v-dialog>
+
                             <v-select class="dialog-input time-input" :rules="rules" label="Vyber čas" v-model="time_start"
                                 :items="timesListForStart">
                             </v-select>
 
-                            <p>—</p>
+                            <p class="time-dash">—</p>
 
                             <v-select class="dialog-input time-input" :rules="rules" label="Vyber čas" v-model="time_end"
                                 :items="timesList2">
                             </v-select>
                         </div>
 
-                        <v-select class="dialog-input" :rules="rules" label="Vyber místo" v-model="place"
+                        <v-select class="dialog-input place-input" :rules="rules" label="Vyber místo" v-model="place"
                             :items="['Online', 'Offline (lektorova lokace)']"></v-select>
 
-                    </div>
+                        <div class="tags-card" :class="{ 'tags-disabled': formDisabled }">
 
-                    <div class="tags-card form-container">
+                            <v-card-title class="tags-title">
+                                <p>Čeho se lekce bude týkat?</p>
+                            </v-card-title>
 
-                        <v-card-title class="tags-title">
-                            <p>Vyber štítky</p>
-                        </v-card-title>
-
-                        <div class="tags-container" v-if="tagsSelected.length">
-                            <div class="tag selected" @click="removeTag(tag.name)" @mouseenter="tag.mouseover = true"
-                                @mouseleave="tag.mouseover = false" v-for="tag in tagsSelected">
-                                <p>{{ tag.name }} <span v-if="tag.mouseover">X</span></p>
+                            <div class="tags-container" v-if="tagsSelected.length">
+                                <p class="tags-label">Vybrané:</p>
+                                <div class="tag selected" @click="removeTag(tag.name)" @mouseenter="tag.mouseover = true"
+                                    @mouseleave="tag.mouseover = false" v-for="tag in tagsSelected">
+                                    <p>{{ tag.name }} <span v-if="tag.mouseover">X</span></p>
+                                </div>
+                            </div>
+                            <br v-if="tagsSelected.length">
+                            <div class="tags-container">
+                                <p class="tags-label">Dostupné:</p>
+                                <div class="tag" @click="addTag(tag.name)" v-for="tag in props.lecturer.tags">
+                                    <p>{{ tag.name }}</p>
+                                </div>
                             </div>
                         </div>
-                        <br v-if="tagsSelected.length">
-                        <div class="tags-container">
-                            <div class="tag" @click="addTag(tag.name)" v-for="tag in props.lecturer.tags">
-                                <p>{{ tag.name }}</p>
-                            </div>
-                        </div>
+
                     </div>
+
                     <button class="dialog-btn" type="submit">
-                        <p>Odeslat</p>
+                        <p>Potvrdit</p>
                     </button>
+                    <v-progress-circular class="spinner" v-if="formDisabled" indeterminate></v-progress-circular>
                 </v-form>
             </v-card-item>
 
@@ -211,12 +226,12 @@ async function postReservation(e) {
 
 .dialog {
     width: 80vw;
-    height: 80vh;
+    height: 72vh;
 }
 
 .dialog-card {
     width: 80vw;
-    height: 80vh;
+    height: 72vh;
 
     background-color: var(--white);
     box-shadow: 1px 1px 3px var(--jet);
@@ -238,17 +253,17 @@ async function postReservation(e) {
 
 .dialog-title {
     position: relative;
-    top: 2vh;
-    left: 1vw;
+    top: 2.5vh;
+    left: 1.1vw;
 
-    font-size: 1.6rem;
+    font-size: 1.7rem;
 
-    margin-bottom: 5vh;
+    margin-bottom: 3.7vh;
 }
 
 .form {
     width: 80vw;
-    height: 60vh;
+    height: 55vh;
 
     display: flex;
 }
@@ -257,8 +272,10 @@ async function postReservation(e) {
     flex-grow: 1;
     position: relative;
     width: 9vw;
+
     margin: 1vw;
     margin-top: 0;
+    margin-bottom: 0;
 }
 
 .date-picker {
@@ -270,16 +287,16 @@ async function postReservation(e) {
 
 .dialog-input {
     margin: 1vw;
+    height: 8vh;
 }
 
 .dialog-btn {
     position: fixed;
-    bottom: 5vh;
-    left: 50%;
-    transform: translateX(-50%);
+    bottom: 4vh;
+    left: 2vw;
 
-    width: 30vw;
-    height: 7vh !important;
+    width: 18vw;
+    height: 7.5vh !important;
 
     background-color: var(--sunglow);
     border-radius: 4px;
@@ -304,7 +321,10 @@ async function postReservation(e) {
 }
 
 .tags-container {
+    width: 35vw;
+
     margin: 1vw;
+    margin-bottom: 0;
 
     display: inline-flex;
     flex-wrap: wrap;
@@ -312,9 +332,11 @@ async function postReservation(e) {
 
 .tag {
     width: auto;
-    height: 4.6vh;
+    height: 4.4vh;
 
     padding: 1vh;
+    padding-left: 0.8vw;
+    padding-right: 0.8vw;
     margin: 0.5vh;
 
     border: 3px solid var(--sky-blue);
@@ -327,6 +349,13 @@ async function postReservation(e) {
     cursor: pointer;
 }
 
+
+.tag p {
+    position: relative;
+    top: 50%;
+    transform: translateY(-52%);
+}
+
 .selected p span {
     margin-left: 0.6vw;
 }
@@ -337,12 +366,24 @@ async function postReservation(e) {
 }
 
 .tags-title {
+    width: 35vw;
+
     margin-left: 0.7vw;
     margin-top: 1vh;
 }
 
+.tags-disabled {
+    opacity: 70%;
+}
+
+.tags-label {
+    margin-top: 1.2vh;
+    margin-bottom: 2vh;
+    margin-right: 1vw;
+}
+
 .card-item {
-    padding: 0;
+    padding: 0 !important;
 }
 
 .times-container {
@@ -350,16 +391,42 @@ async function postReservation(e) {
     position: relative;
     left: 50%;
     transform: translateX(-50%);
+
+    width: 36vw;
 }
 
 .time-input {
-    width: 9.1vw;
-    margin-top: 0;
-    margin-bottom: 0;
+    width: 7vw;
+}
+
+.time-input:last-child {
+    margin-right: 0;
+}
+
+.time-dash {
+    margin: 0.2vw !important;
+    margin-top: 3.5vh !important;
+}
+
+.date-input {
+    width: 13vw;
+    margin-left: 0;
+    margin-right: 2.5vw;
 }
 
 .times-container p {
     font-size: 1.2rem;
     margin: 1vh;
+}
+
+.place-input {
+    margin-top: 0;
+}
+
+.spinner {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
